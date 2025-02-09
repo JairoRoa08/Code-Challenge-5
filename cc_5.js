f4a2e7a0dacf6779c60f267925d05213ac612e07
Jairo Roa <Jroa@usf.edu>
2025-02-09 15:33:39 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties
+const customer = {
+    name: "Jimmy Butler",
+    age: 33,
+    email: "JButler@gmail.com"
+};
+
+console.log('Name: ${customer.name}');
+console.log('Age: ${customer.age}');
+console.log('Email: ${customer.email}');
